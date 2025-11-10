bd8396fa3ed923154610559a60ba702c9054a59a
Claudiana27 <nomenjanaharydiana27@gmail.com>
2025-11-10 09:36:03 +0300

    Initial commit - backend projet gestion des poubelles

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+import connection from './db.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Backend fonctionnel'));
+
+app.listen(PORT, () => console.log(`Serveur Node.js démarré sur http://localhost:${PORT}`));
---
+++ src/db.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: true }
+});
+
+connection.connect(err => {
+  if (err) console.error('Erreur de connexion MySQL :', err);
+  else console.log('Connexion réussie à la base Railway ✅');
+});
+
+export default connection;
